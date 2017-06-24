037ca445cbccc286c4b933d9875250ac624ed50d
Bu Kinoshita <bukinoshita@gmail.com>
2017-06-24 09:54:20 -0400

    minor tweak

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fs = require('fs')
 const writeLicense = require('./lib/write-license')
 
-const l = (type = 'MIT', name, opts = {}) => {
+module.exports = (type = 'MIT', name, opts = {}) => {
   return new Promise((resolve, reject) => {
     if (typeof type !== 'string' || typeof name !== 'string') {
       reject(new TypeError('Type and name must be a string.'))
